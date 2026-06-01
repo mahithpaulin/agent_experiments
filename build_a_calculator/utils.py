fc9fa8ada5bc1b8f00d9e650329637707cea6975
mahithpaulin <mahithpaulin@gmail.com>
2026-06-01 11:21:03 +0530

    agent1.5:build_a_calculator/utils.py

--- build_a_calculator/utils.py
+++ build_a_calculator/utils.py
@@ -1,13 +1,13 @@
-def add(num1, num2):
-    return num1 + num2
+def add(a, b):
+    return a + b
 
-def subtract(num1, num2):
-    return num1 - num2
+def subtract(a, b):
+    return a - b
 
-def multiply(num1, num2):
-    return num1 * num2
+def multiply(a, b):
+    return a * b
 
-def divide(num1, num2):
-    if num2 == 0:
-        raise ZeroDivisionError("Cannot divide by zero")
-    return num1 / num2
+def divide(a, b):
+    if b == 0:
+        return None
+    return a / b
