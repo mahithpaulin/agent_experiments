462aebfceca1378aaed9d155b7e0243e662eee3d
mahithpaulin <mahithpaulin@gmail.com>
2026-06-02 13:22:10 +0530

    agent1.5:create_a_small_programming_language_and/test_main.py

--- create_a_small_programming_language_and/test_main.py
+++ create_a_small_programming_language_and/test_main.py
@@ -1,176 +1,105 @@
 import pytest
-from main import compile_source, Lexer, Parser, Compiler
+from compiler import compile_source, LexerError, ParserError, CompilerError
 
-def run_compiler_and_exec(source):
+def test_var_decl_and_assign():
+    source = """
+    var x = 10;
+    x = x + 5;
+    """
+    pycode = compile_source(source)
+    # Execute generated code and check variable x
+    local_env = {}
+    exec(pycode, {}, local_env)
+    assert local_env["x"] == 15
+
+def test_if_else():
+    source = """
+    var x = 10;
+    if (x > 5) {
+        x = 1;
+    } else {
+        x = 2;
+    }
+    """
     pycode = compile_source(source)
     local_env = {}
     exec(pycode, {}, local_env)
-    return local_env
-
-def test_variable_assignment_and_arithmetic():
-    source = """
-    a = 5;
-    b = 10;
-    c = a + b * 2;
-    """
-    env = run_compiler_and_exec(source)
-    assert env['a'] == 5
-    assert env['b'] == 10
-    assert env['c'] == 5 + 10 * 2
-
-def test_if_else_statement():
-    source = """
-    a = 10;
-    if (a > 5) {
-        b = 1;
-    } else {
-        b = 2;
-    }
-    """
-    env = run_compiler_and_exec(source)
-    assert env['b'] == 1
-
-    source2 = """
-    a = 3;
-    if (a > 5) {
-        b = 1;
-    } else {
-        b = 2;
-    }
-    """
-    env2 = run_compiler_and_exec(source2)
-    assert env2['b'] == 2
+    assert local_env["x"] == 1
 
 def test_while_loop():
     source = """
-    i = 0;
-    s = 0;
+    var x = 0;
+    var i = 0;
     while (i < 5) {
-        s = s + i;
+        x = x + i;
         i = i + 1;
     }
     """
-    env = run_compiler_and_exec(source)
-    assert env['s'] == sum(range(5))
-    assert env['i'] == 5
+    pycode = compile_source(source)
+    local_env = {}
+    exec(pycode, {}, local_env)
+    assert local_env["x"] == 10  # 0+1+2+3+4=10
 
-def test_function_definition_and_call():
+def test_function_def_and_call():
     source = """
-    def add(x, y) {
-        return x + y;
+    function add(a, b) {
+        return a + b;
     }
-    a = add(3, 4);
+    var result = add(3, 4);
     """
-    env = run_compiler_and_exec(source)
-    assert env['a'] == 7
-    assert callable(env['add'])
+    pycode = compile_source(source)
+    local_env = {}
+    exec(pycode, {}, local_env)
+    assert local_env["result"] == 7
 
-def test_function_without_return():
+def test_nested_function_and_conditionals():
     source = """
-    def foo() {
-        a = 5;
-    }
-    b = foo();
-    """
-    env = run_compiler_and_exec(source)
-    assert env['b'] is None
-
-def test_nested_function_calls_and_conditionals():
-    source = """
-    def max(a, b) {
-        if (a > b) {
-            return a;
+    function factorial(n) {
+        if (n == 0) {
+            return 1;
         } else {
-            return b;
+            return n * factorial(n - 1);
         }
     }
-    x = max(10, 20);
-    y = max(5, 3);
+    var f5 = factorial(5);
     """
-    env = run_compiler_and_exec(source)
-    assert env['x'] == 20
-    assert env['y'] == 5
+    pycode = compile_source(source)
+    local_env = {}
+    exec(pycode, {}, local_env)
+    assert local_env["f5"] == 120
 
-def test_boolean_values_and_comparisons():
+def test_unary_operations():
     source = """
-    a = true;
-    b = false;
-    c = (5 == 5);
-    d = (5 != 3);
-    e = (4 < 3);
-    f = (4 <= 4);
-    g = (5 > 2);
-    h = (5 >= 5);
+    var x = -5;
+    var y = +10;
+    var z = -x + y;
     """
-    env = run_compiler_and_exec(source)
-    assert env['a'] is True
-    assert env['b'] is False
-    assert env['c'] is True
-    assert env['d'] is True
-    assert env['e'] is False
-    assert env['f'] is True
-    assert env['g'] is True
-    assert env['h'] is True
+    pycode = compile_source(source)
+    local_env = {}
+    exec(pycode, {}, local_env)
+    assert local_env["x"] == -5
+    assert local_env["y"] == 10
+    assert local_env["z"] == 15
 
-def test_unary_minus_and_plus():
+def test_syntax_error():
     source = """
-    a = -5;
-    b = +10;
-    c = -(-3);
-    d = +(+4);
+    var x = 10
     """
-    env = run_compiler_and_exec(source)
-    assert env['a'] == -5
-    assert env['b'] == 10
-    assert env['c'] == 3
-    assert env['d'] == 4
+    with pytest.raises(ParserError):
+        compile_source(source)
 
-def test_function_call_as_expression():
+def test_lexer_error():
     source = """
-    def square(x) {
-        return x * x;
-    }
-    a = square(3) + square(4);
+    var x = 10;
+    var y = @;
     """
-    env = run_compiler_and_exec(source)
-    assert env['a'] == 9 + 16
+    with pytest.raises(LexerError):
+        compile_source(source)
 
-def test_parser_errors():
-    # Missing semicolon
-    source = "a = 5"
-    with pytest.raises(Exception):
-        lexer = Lexer(source)
-        parser = Parser(lexer)
-        ast = parser.program()
-
-    # Invalid token
-    source2 = "a = 5 $;"
-    with pytest.raises(Exception):
-        lexer = Lexer(source2)
-        parser = Parser(lexer)
-        ast = parser.program()
-
-def test_empty_program():
-    source = ""
-    env = run_compiler_and_exec(source)
-    assert isinstance(env, dict)
-
-def test_return_without_expression():
+def test_unknown_operator_error():
     source = """
-    def foo() {
-        return 5;
-    }
-    a = foo();
+    var x = 10;
+    x = x ^ 2;
     """
-    env = run_compiler_and_exec(source)
-    assert env['a'] == 5
-
-def test_multiple_statements():
-    source = """
-    a = 1;
-    b = 2;
-    c = 3;
-    d = a + b + c;
-    """
-    env = run_compiler_and_exec(source)
-    assert env['d'] == 6
+    with pytest.raises(CompilerError):
+        compile_source(source)
